7eff1d7d9dd0ae4c77fb14ed683d64283ae06e3f
lop.py <100444985+lop-p@users.noreply.github.com>
2022-04-26 09:18:50 -0500

    dotCats

---
+++ dotCats/main.py
@@ -0,0 +1,9 @@
+print("dotCats 0.1");
+def DotCats():
+    DotCon = True;
+    print("DotCon == True");
+
+q1 = input("Question 1: How do you p2]42\n")
+if q1 == "p2]42":
+    print("True!")
+    q2 = input("Questipn 2 Do you")
